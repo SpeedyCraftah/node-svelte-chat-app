7f67e779eb917f7ed0ad7f1380eb829723f464c8
SpeedyCraftah <45142584+SpeedyCraftah@users.noreply.github.com>
2024-06-29 13:42:28 +0100

    create a mock user generator

--- backend/populateDB.js
+++ backend/populateDB.js
@@ -2,7 +2,9 @@
 // To be used after changes to database structure or after erasing a database.
 
 const db = require("./handlers/database");
+const { faker } = require('@faker-js/faker');
 
+// Create some base accounts.
 (async() => {
     // Create an account for the login user.
     const loginAccount = await db.users.create(1, "Speedy", "speedy", "speedy123");
@@ -15,4 +17,17 @@ const db = require("./handlers/database");
     // Create a DM between login and target user.
     const dm = db.dm_channels.create(loginAccount.id, targetUser.id);
     console.log("DM:", dm);
+});
+
+// Create some mock accounts for testing involving multiple users.
+(async() => {
+    for (let i = 0; i < 100; i++) {
+        const name = faker.person.firstName();
+        const username = faker.internet.userName({ firstName: name });
+        const password = faker.internet.password({ memorable: true });
+        const avatar = faker.image.avatar();
+
+        const account = await db.users.create(1, name, username, password, avatar);
+        console.log("Created account:", account);
+    }
 })();
